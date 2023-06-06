e911784eb298b5c58ed46beb10e5c5b948b558de
KosterKI <132550158+KosterKI@users.noreply.github.com>
2023-06-06 13:29:48 +0300

    Add files via upload

---
+++ untitled4/src/Items.java
@@ -0,0 +1,15 @@
+public class Items {
+    private String name;
+    private int price;
+
+    public Items(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getPrice(){
+        return price;
+    }
+}
